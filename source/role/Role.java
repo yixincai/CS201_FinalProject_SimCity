ff67ba7da9d55b764b3678b98574fe166550a385
Eric Gauderman <gauderma@usc.edu>
2013-11-17 19:59:11 -0800

    Added Role class. close #9

--- source/role/Role.java
+++ source/role/Role.java
@@ -1,5 +1,22 @@
 package role;
 
-public class Role {
-	//TODO see Agent.java for guidelines
+import agent.PersonAgent;
+
+public abstract class Role
+{
+	// ----------------------------- DATA ------------------------------------
+	private PersonAgent _person;
+	public boolean _active = false;
+	
+	// ----------------------------- ACCESSORS ------------------------------------
+	public void setPersonAgent(PersonAgent person) { _person = person; }
+	
+	
+	
+	// ----------------------------- METHODS ------------------------------------
+	protected void stateChanged()
+	{
+		if(_active) _person.stateChanged();
+	}
+	public abstract boolean pickAndExecuteAnAction();
 }
