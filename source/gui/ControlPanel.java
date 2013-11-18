317b12b9f29fcbe9161a7e86ee04befd48a4d839
Tanner Zigrang <tzigrang@usc.edu>
2013-11-18 13:16:11 -0800

    Created the basic Current Building Panel.  Each specific type of building will look slightly different in order to display the pertenant information.  Issue #8.

--- source/gui/ControlPanel.java
+++ source/gui/ControlPanel.java
@@ -21,7 +21,7 @@ public class ControlPanel extends JTabbedPane {
 		//This is all placeholder code just to get the panels into tabs.  Each tab will have its own class eventually.
 		CreatePersonPanel NewPersonPanel = new CreatePersonPanel();
 		JPanel CurrentPersonPanel = new JPanel();
-		JPanel CurrentBuildingPanel = new JPanel();
+		CurrentBuildingPanel CurrentBuildingPanel = new CurrentBuildingPanel();
 		this.addTab("Current Person", null, CurrentPersonPanel, "Not Yet Implemented");
 		this.addTab("Current Building", null, CurrentBuildingPanel, "Not Yet Implemented");
 		this.addTab("New Person", null, NewPersonPanel, "Not Yet Implemented");
