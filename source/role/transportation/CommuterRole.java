9864d357333d9a27ac9153fac568c329245d5266
Eric Gauderman <gauderma@usc.edu>
2013-11-17 21:46:02 -0800

    Added Place class, city package, and CommuterRole role.

---
+++ source/role/transportation/CommuterRole.java
@@ -0,0 +1,15 @@
+package role.transportation;
+
+import city.Place;
+import role.Role;
+
+public class CommuterRole extends Role {
+	public Place destination;
+
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
