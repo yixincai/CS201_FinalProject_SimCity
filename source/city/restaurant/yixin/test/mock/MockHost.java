9eaa0fd13757d45b00a8674860c507200e2f5071
yixincai <yixincai@usc.edu>
2013-11-19 21:20:07 -0800

    add original files to restaurant for hierarchy purpose

---
+++ source/city/restaurant/yixin/test/mock/MockHost.java
@@ -0,0 +1,23 @@
+package city.restaurant.yixin.test.mock;
+
+import restaurant.interfaces.*;
+
+public class MockHost extends Mock implements Host{
+	public EventLog log = new EventLog();
+
+	public MockHost(String name) {
+		super(name);
+	}
+	
+	public void msgIWantFood(Customer cust, int count){}
+
+	public void msgIAmLeaving(Customer cust){}
+	
+	public void msgIWantToStay(Customer cust){}
+
+	public void msgTableIsFree(Customer cust, int tablenumber){}
+	
+	public void msgWantToBreak(Waiter w){}
+	
+	public void msgWantToComeBack(Waiter w){}
+}
