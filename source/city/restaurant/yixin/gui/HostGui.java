131a4f27788e847caa9c795423cfe91ee3eb7db4
yixincai <yixincai@usc.edu>
2013-11-19 21:07:04 -0800

    add initial code to the restaurant without changing

---
+++ source/city/restaurant/yixin/gui/HostGui.java
@@ -0,0 +1,53 @@
+package city.restaurant.yixin.gui;
+
+import restaurant.HostAgent;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class HostGui extends JPanel implements Gui {
+
+    private HostAgent agent = null;
+
+    private int xPos = 50, yPos = 20;//default waiter position
+    public static int xGap = 40;
+    public static int yGap = 40;
+    private String WA = "Waiting Area";
+    
+    //private ImageIcon i = new ImageIcon("image/host.png");
+    //private Image image = i.getImage();
+    public HostGui(HostAgent agent) {
+        this.agent = agent;
+    }
+
+    public void updatePosition() {
+    }
+
+    public void draw(Graphics2D g) {
+    	g.setColor(Color.BLUE);
+    	g.drawString(WA, 30, 10);
+    }
+
+    public boolean isPresent() {
+        return true;
+    }
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public int getYPos() {
+        return yPos;
+    }
+    
+    public void pauseThread(){
+    	if (agent != null)
+    		agent.pauseThread();
+    }
+    
+    public void resumeThread(){
+    	if (agent != null)
+    		agent.resumeThread();
+    }
+}
