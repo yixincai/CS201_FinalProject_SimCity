d4e1b3dfff3b4b1974b4950d6259e1cc059949c3
tzigrang <tzigrang@usc.edu>
2013-11-21 11:04:01 -0800

    Adding the slider for money so we don't have to worry about users iputing somethig other than a float for money.

--- source/gui/CreatePersonPanel.java
+++ source/gui/CreatePersonPanel.java
@@ -15,10 +15,11 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
-public class CreatePersonPanel extends JPanel implements ActionListener 
+public class CreatePersonPanel extends JPanel implements ActionListener
 {
 	
 	public CreatePersonPanel()
@@ -29,10 +30,12 @@ public class CreatePersonPanel extends JPanel implements ActionListener
 		String[] occupations = {"None", "Waiter", "Restaurant Cashier", "Cook", "Restaurant Host", 
 								"Bank Teller", "Bank Host", "Market Cashier", "Market Employee"};
 		JTextField nameField = new JTextField(15);
-		JTextField moneyField = new JTextField(15);
 		JComboBox occupationBox = new JComboBox(occupations);
 		JLabel nameLabel = new JLabel("Name:");
 		JLabel moneyLabel = new JLabel("Money:");
+		JSlider moneySlider = new JSlider(JSlider.HORIZONTAL, 0, 1000, 500);
+		moneySlider.setMajorTickSpacing(25);
+		JLabel moneyField = new JLabel("$500.00" );
 		JLabel occupationLabel = new JLabel("Occupation:");
 		this.add(nameLabel);
 		this.add(moneyLabel);
@@ -40,6 +43,7 @@ public class CreatePersonPanel extends JPanel implements ActionListener
 		this.add(nameField);
 		this.add(moneyField);
 		this.add(occupationBox);
+		this.add(moneySlider);
 		//Position NameLabel
 		layout.putConstraint(SpringLayout.WEST, nameLabel, 5, SpringLayout.WEST, this);
 		layout.putConstraint(SpringLayout.NORTH, nameLabel, 10, SpringLayout.NORTH, this);
@@ -51,13 +55,17 @@ public class CreatePersonPanel extends JPanel implements ActionListener
 		layout.putConstraint(SpringLayout.NORTH, moneyLabel, 15, SpringLayout.SOUTH, nameLabel);
 		//Position MoneyField
 		layout.putConstraint(SpringLayout.EAST, moneyField, -5, SpringLayout.EAST, this);
-		layout.putConstraint(SpringLayout.NORTH, moneyField, 10, SpringLayout.SOUTH, nameField);
+		layout.putConstraint(SpringLayout.NORTH, moneyField, 15, SpringLayout.SOUTH, nameLabel);
 		//Position OccupationLabel
 		layout.putConstraint(SpringLayout.WEST, occupationLabel, 5, SpringLayout.WEST, this);
 		layout.putConstraint(SpringLayout.NORTH, occupationLabel, 15, SpringLayout.SOUTH, moneyLabel);
 		//Position OccupationBox
 		layout.putConstraint(SpringLayout.EAST, occupationBox, -5, SpringLayout.EAST, this);
-		layout.putConstraint(SpringLayout.NORTH, occupationBox, 10, SpringLayout.SOUTH, moneyField);
+		layout.putConstraint(SpringLayout.NORTH, occupationBox, 10, SpringLayout.SOUTH, moneyLabel);
+		//Position MoneySlider
+		layout.putConstraint(SpringLayout.NORTH, moneySlider, 15, SpringLayout.SOUTH, occupationBox);
+		layout.putConstraint(SpringLayout.WEST, moneySlider, 5, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, moneySlider, -5, SpringLayout.EAST, this);
 		
 		JButton addButton = new JButton("Add");
 		//addButton.setPreferredSize(new Dimension(1024/3 - 17, 30));
