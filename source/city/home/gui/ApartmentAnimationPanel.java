93feca797f8e25a87d396bd93d7e868e1cb8ccec
Omar Khulusi <okhulusi@usc.edu>
2013-11-24 03:29:28 -0800

    Reformatting apartment floor plan

--- source/city/home/gui/ApartmentAnimationPanel.java
+++ source/city/home/gui/ApartmentAnimationPanel.java
@@ -17,24 +17,35 @@ import city.bank.gui.BankHostRoleGui;
 public class ApartmentAnimationPanel extends JPanel implements ActionListener {
     private final int BEDX = 600;
     private final int BEDY = 300;
+    private final int STOVEX = 570;
+    private final int STOVEY = 300;
+    private final int FRIDGEX = 550;
+    private final int FRIDGEY = 300;
+    
+    private final int BEDX2 = 600;
+    private final int BEDY2 = 100;
+    private final int STOVEX2 = 570;
+    private final int STOVEY2 = 100;
+    private final int FRIDGEX2 = 550;
+    private final int FRIDGEY2 = 100;
+    
+    private final int BEDX3 = 100;
+    private final int BEDY3 = 300;
+    private final int STOVEX3 = 130;
+    private final int STOVEY3 = 300;
+    private final int FRIDGEX3 = 150;
+    private final int FRIDGEY3 = 300;
+    
+    private final int BEDX4 = 100;
+    private final int BEDY4 = 100;
+    private final int STOVEX4 = 130;
+    private final int STOVEY4 = 100;
+    private final int FRIDGEX4 = 150;
+    private final int FRIDGEY4 = 100;
     
     private final int BEDWIDTH = 30;
     private final int BEDHEIGHT = 30;
-    
-    private final int BEDWALLX = 400;
-    private final int BEDWALLY = 0;
-    private final int BEDWALLWIDTH = 10;
-    private final int BEDWALLHEIGHT = 160;
-    
-    private final int BEDWALLX2 = 400;
-    private final int BEDWALLY2 = 200;
-    
-    private final int STOVEX = 200;
-    private final int STOVEY = 30;
     private final int STOVEDIM = 20;
-    
-    private final int FRIDGEX = 220;
-    private final int FRIDGEY = 30;
     private final int FRIDGEDIM = 20;
 
     private final int WINDOWX = 682;
@@ -65,16 +76,21 @@ public class ApartmentAnimationPanel extends JPanel implements ActionListener {
 
         g2.setColor(Color.CYAN);
         g2.fillRect(BEDX, BEDY, BEDWIDTH, BEDHEIGHT);
+        g2.fillRect(BEDX2, BEDY2, BEDWIDTH, BEDHEIGHT);
+        g2.fillRect(BEDX3, BEDY3, BEDWIDTH, BEDHEIGHT);
+        g2.fillRect(BEDX4, BEDY4, BEDWIDTH, BEDHEIGHT);
         
         g2.setColor(Color.BLACK);
-        g2.fillRect(BEDWALLX, BEDWALLY, BEDWALLWIDTH, BEDWALLHEIGHT);
-        g2.fillRect(BEDWALLX2, BEDWALLY2, BEDWALLWIDTH, BEDWALLHEIGHT);
-        
-        g2.setColor(Color.CYAN);
         g2.fillRect(STOVEX, STOVEY, STOVEDIM, STOVEDIM);
+        g2.fillRect(STOVEX2, STOVEY2, STOVEDIM, STOVEDIM);
+        g2.fillRect(STOVEX3, STOVEY3, STOVEDIM, STOVEDIM);
+        g2.fillRect(STOVEX4, STOVEY4, STOVEDIM, STOVEDIM);
         
         g2.setColor(Color.WHITE);
         g2.fillRect(FRIDGEX, FRIDGEY, FRIDGEDIM, FRIDGEDIM);
+        g2.fillRect(FRIDGEX2, FRIDGEY2, FRIDGEDIM, FRIDGEDIM);
+        g2.fillRect(FRIDGEX3, FRIDGEY3, FRIDGEDIM, FRIDGEDIM);
+        g2.fillRect(FRIDGEX4, FRIDGEY4, FRIDGEDIM, FRIDGEDIM);
         
 
         for(Gui gui : guis) {
