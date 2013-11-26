e9b7587d49cd3f1c5a9a140513dce95da9c47748
tzigrang <tzigrang@usc.edu>
2013-11-26 05:06:56 -0800

    Incremental push.  I am delusional and don't really know what I'm doing anymore or what I'm pushing. Issue #26.

--- source/city/restaurant/tanner/TannerRestaurantCookRole.java
+++ source/city/restaurant/tanner/TannerRestaurantCookRole.java
@@ -1,7 +1,11 @@
 package city.restaurant.tanner;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Timer;
+import java.util.concurrent.Semaphore;
 
 import city.PersonAgent;
 import city.Place;
@@ -9,6 +13,8 @@ import city.market.Item;
 import city.market.Market;
 import city.market.interfaces.MarketCashier;
 import city.restaurant.RestaurantCookRole;
+import city.restaurant.tanner.MarketOrder.OrderState;
+import city.restaurant.tanner.gui.TannerRestaurantCookRoleGui;
 import city.restaurant.tanner.interfaces.TannerRestaurantCashier;
 import city.restaurant.tanner.interfaces.TannerRestaurantCook;
 import city.restaurant.tanner.interfaces.TannerRestaurantWaiter;
@@ -17,52 +23,112 @@ public class TannerRestaurantCookRole extends RestaurantCookRole implements Tann
 {
 
 //--------------------------------------------Data---------------------------------------------------------------------
-	public TannerRestaurantCashier cashier;
+	List<Order> orders;
+	List<MarketOrder> marketOrders;
+	String name_;
+	Point position;
+	boolean underStocked;
+	Timer cookTimer = new Timer();
+	Semaphore doingAction;
+	TannerRestaurantCookRoleGui myGui;
+	TannerRestaurantCashier cashier;
+	TannerRestaurant restaurant;
+	
 	
 //----------------------------------------Constructors-----------------------------------------------------------------	
-	public TannerRestaurantCookRole(PersonAgent person, TannerRestaurant rest, TannerRestaurantCashierRole cashier) {
+	public TannerRestaurantCookRole(PersonAgent person, TannerRestaurant rest, TannerRestaurantCashierRole cashier, String name)
+	{
 		super(person);
-		// TODO Auto-generated constructor stub
+		name_ = name;
+		orders = Collections.synchronizedList(new ArrayList<Order>());
+		marketOrders = Collections.synchronizedList(new ArrayList<MarketOrder>());
+		doingAction = new Semaphore(0, true);	
+		this.cashier = cashier;
+		restaurant = rest;
+		
 	}
 	
 //-----------------------------------------Accessors------------------------------------------------------------------
 	
 	@Override
-	public Place place() {
-		// TODO Auto-generated method stub
-		return null;
+	public Place place() 
+	{
+		return this.restaurant;
 	}
 	
 	@Override
-	public Point getPosition() {
-		// TODO Auto-generated method stub
+	public Point getPosition() 
+	{
 		return null;
 	}
 
 //------------------------------------------Messages------------------------------------------------------------------
 	
+	
 	@Override
-	public void msgOrderFulfillment(Market m, List<Item> order) {
-		// TODO Auto-generated method stub
+	public void msgHereIsANewOrder(int choice, int tableNumber, TannerRestaurantWaiter w) 
+	{
+		if(restaurant.menu.get(choice).stock > 0)
+		{
+			orders.add(new Order(w, choice, tableNumber));
+			restaurant.menu.get(choice).stock--;
+			stateChanged();
+		}
+		else
+		{
+			w.msgThatChoiceIsOutOfStock(choice, tableNumber);
+		}	
+	}
+	
+	@Override
+	public void msgOrderFulfillment(Market m, List<Item> order) 
+	{
 
 	}
 
 	@Override
-	public void msgThisIsWhatIHave(int[] ItemsGiven, MarketCashier m) {
+	public void msgThisIsWhatIHave(int[] ItemsGiven, MarketCashier m)
+	{
+		synchronized(marketOrders)
+		{
+			for(int i = 0; i < marketOrders.size(); i++)
+			{
+				if(marketOrders.get(i).market == m)
+				{
+					marketOrders.get(i).whatIGot = ItemsGiven;
+					marketOrders.get(i).orderState = OrderState.orderRecieved;
+					for(int j = 0; j < marketOrders.get(i).whatIGot.length; j++)
+					{
+						restaurant.menu.get(j).stock += marketOrders.get(i).whatIGot[j];
+					}
+					stateChanged();
+				}
+			}
+		}		
+	}
+
+	@Override
+	public void msgAllOutOfGoods(MarketCashier m) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void msgAllOutOfGoods(MarketCashier m) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void msgHereIsTheBill(float amount, MarketCashier m) {
-		// TODO Auto-generated method stub
-		
+	public void msgHereIsTheBill(float amount, MarketCashier m) 
+	{
+		synchronized(marketOrders)
+		{
+			for(int i = 0; i < marketOrders.size(); i++)
+			{
+				if(marketOrders.get(i).market == m)
+				{
+					marketOrders.get(i).orderState = OrderState.readyToPay;
+					marketOrders.get(i).cost = amount;
+					stateChanged();
+				}
+			}
+		}		
 	}
 
 	
@@ -85,10 +151,24 @@ public class TannerRestaurantCookRole extends RestaurantCookRole implements Tann
 
 	}
 
-	@Override
-	public void msgHereIsANewOrder(int choice, int tableNumber,
-			TannerRestaurantWaiter w) {
-		// TODO Auto-generated method stub
-		
+
+}
+
+//------------------------------------------Utilities-----------------------------------------------------------------
+
+class MarketOrder
+{
+	Market market;
+	int[] whatIOrdered;
+	int[] whatIGot;
+	float cost;
+	public enum OrderState {orderSubmitted, orderRecieved, readyToPay}
+	OrderState orderState;
+	
+	public MarketOrder(int[] order, Market m)
+	{
+		whatIOrdered = order;
+		market = m;
+		orderState = OrderState.orderSubmitted;
 	}
 }
