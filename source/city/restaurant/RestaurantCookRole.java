578cba8af1f8b61d76a24641cbce3ff8f750dcbc
Eric Gauderman <gauderma@usc.edu>
2013-12-03 22:10:23 -0800

    Basic refactor of WaiterAgent to EricWaiterRole

--- source/city/restaurant/RestaurantCookRole.java
+++ source/city/restaurant/RestaurantCookRole.java
@@ -11,7 +11,6 @@ public abstract class RestaurantCookRole extends Role{
 
 	public RestaurantCookRole(PersonAgent person) {
 		super(person);
-		// TODO Auto-generated constructor stub
 	}
 	public abstract void msgOrderFulfillment(Market m, List<Item> order);
 
