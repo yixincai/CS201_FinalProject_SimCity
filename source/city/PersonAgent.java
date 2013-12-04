c6d49a272056eb01038d876cead1e06eca9d9642
yixincai <yixincai@usc.edu>
2013-12-04 13:09:10 -0800

    solved image issue and there should be some problem with workingDay in perosnAgent

--- source/city/PersonAgent.java
+++ source/city/PersonAgent.java
@@ -509,10 +509,10 @@ public class PersonAgent extends Agent implements Person
 	// ------------------------------------------ UTILITIES -------------------------------------
 	private boolean workingToday()
 	{
-		//return true;
+		return true;
 		// Commenting this out because we're currently not taking account of weekends
-		return ((_state.today() == Time.Day.SATURDAY || _state.today() == Time.Day.SUNDAY) && !_weekday_notWeekend) ||
-				(!(_state.today() == Time.Day.SATURDAY || _state.today() == Time.Day.SUNDAY) && _weekday_notWeekend);
+//		return ((_state.today() == Time.Day.SATURDAY || _state.today() == Time.Day.SUNDAY) && !_weekday_notWeekend) ||
+//				(!(_state.today() == Time.Day.SATURDAY || _state.today() == Time.Day.SUNDAY) && _weekday_notWeekend);
 				
 	}
 	private boolean timeToBeAtWork()
