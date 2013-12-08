b658d803ce22ce7462e8f99d32f5becdddb166de
yixincai <yixincai@usc.edu>
2013-12-07 16:49:53 -0800

    figured out the route finding strategy of cars

--- source/city/transportation/gui/CommuterGui.java
+++ source/city/transportation/gui/CommuterGui.java
@@ -1,7 +1,6 @@
 package city.transportation.gui;
 
 import java.awt.Color;
-
 import java.awt.Graphics2D;
 import java.util.List;
 import java.awt.Point;
@@ -20,7 +19,7 @@ public class CommuterGui implements Gui {
 
 	private static final int NULL_POSITION_X = 300;
 	private static final int NULL_POSITION_Y = 300;
-	
+
 
 	int _xPos, _yPos;
 	int _currentBlockX = 0, _currentBlockY = 0;//TODO set the block positions used by cars
@@ -33,53 +32,54 @@ public class CommuterGui implements Gui {
 	Command _transportationMethod = Command.none;
 	boolean isPresent = true;
 	private Semaphore _reachedDestination = new Semaphore(0, true);
-
+	private Semaphore _delayForMoving = new Semaphore(0, true);
+	private Timer _lookUpDelay = new Timer();
 	CommuterRole _commuter;
 	AStarTraversal _aStarTraversal;
-	
+
 	Position currentPosition;
-	
+
 	//----------------------------------Constructor & Setters & Getters----------------------------------
 	public CommuterGui(CommuterRole commuter, Place initialPlace) {
 		System.out.println("Created CommuterGui");
 		// Note: placeX and placeY can safely receive values of null
-	/*	_xPos = placeX(initialPlace);
+		/*	_xPos = placeX(initialPlace);
 		_yPos = placeY(initialPlace); */
 		_commuter = commuter;
 		//currentPosition = convertPixelToGridSpace(placeX(initialPlace), placeY(initialPlace));
 	}
-	
+
 	public double getManhattanDistanceToDestination(Place destination){
 		double x = Math.abs(placeX(destination) - currentPosition.getX());
 		double y = Math.abs(placeY(destination) - currentPosition.getY());
-		
+
 		return x+y;
 	}
-	
+
 	public void releaseSemaphore(){ _reachedDestination.release(); };
 
 	@Override
 	public boolean isPresent() {
 		return isPresent;
 	}
-	
+
 	public void setPresent(boolean present){
 		this.isPresent = present;
 	}
-	
+
 	public void setXY(int x, int y){
 		_xPos = x;
 		_yPos = y;
 	}
-	
+
 	public int getX(){
 		return currentPosition.getX();
 	}
-	
+
 	public int getY(){
 		return currentPosition.getY();
 	}
-	
+
 	//Walking gui-------------------------------------------------------------------------------------------
 	public void walkToLocation(Place destination){
 		// set current x & y to _commuter.currrentPlace()
@@ -89,50 +89,105 @@ public class CommuterGui implements Gui {
 		Position destinationP = convertPixelToGridSpace(placeX(destination), placeY(destination) - 10); // offset by 10
 		guiMoveFromCurrentPositionTo(destinationP);
 	}
-	
+
 	public void driveToLocation(Place destination){
 		// set current x & y to _commuter.currrentPlace()
 		// set visible to true
 		route.clear();
 		setPresent(true);
-//		_xDestination = placeX(destination);
-//		_yDestination = placeY(destination);
-		_destinationBlockX = getBlockX(_xDestination);
-		_destinationBlockY = getBlockY(_yDestination);
-//		while (_currentBlockX != _destinationBlockX){
-//			if (_destinationBlockX > _currentBlockX){
-//				route.add(new Point(_currentBlockX, _currentBlockY));
-//				_currentBlockX++;
-//			}
-//			else {
-//				route.add(new Point(_currentBlockX, _currentBlockY));
-//				_currentBlockX--;
-//			}
-//		}
-//		while (_currentBlockY != _destinationBlockY){
-//			if (_destinationBlockY > _currentBlockY){
-//				route.add(new Point(_currentBlockX, _currentBlockY));
-//				_currentBlockY++;
-//			}
-//			else {
-//				route.add(new Point(_currentBlockX, _currentBlockY));
-//				_currentBlockY--;
-//			}
-//		}
-		
-		route.add(0);
-		intersections.add(1);
-		route.add(1);
-		intersections.add(2);
-		route.add(2);
-		intersections.add(3);
-		route.add(3);
+		_destinationBlockX = getBlockX(placeX(destination));
+		_destinationBlockY = getBlockY(placeY(destination));
+		if (_destinationBlockX == _currentBlockX && _destinationBlockY == _currentBlockY){
+			return;
+		}
+		route.add(_currentBlockX + 3 * _currentBlockY);
+		if (_currentBlockY == 0){
+			if ( _destinationBlockX > _currentBlockX){ //going right
+				intersections.add(_currentBlockX + _currentBlockY);
+				_currentBlockY++;
+				route.add(_currentBlockX + 3 * _currentBlockY);
+				while (_currentBlockX < _destinationBlockX){
+					intersections.add(_currentBlockX + _currentBlockY);
+					_currentBlockX++;
+					route.add(_currentBlockX + 3 * _currentBlockY);
+				}
+				if (_currentBlockY > _destinationBlockY){
+					intersections.add(_currentBlockX + _currentBlockY);
+					_currentBlockY--;
+					route.add(_currentBlockX + 3 * _currentBlockY);
+				}
+			}
+			else{//going left or down
+				while (_currentBlockX > _destinationBlockX){
+					intersections.add(_currentBlockX + _currentBlockY);
+					_currentBlockX--;
+					route.add(_currentBlockX + 3 * _currentBlockY);
+				}
+				if (_currentBlockY < _destinationBlockY){
+					intersections.add(_currentBlockX + _currentBlockY);
+					_currentBlockY++;
+					route.add(_currentBlockX + 3 * _currentBlockY);
+				}
+			}	
+		}
+		else if (_currentBlockY == 1){
+			if ( _destinationBlockX > _currentBlockX){ //going right
+				while (_currentBlockX < _destinationBlockX){
+					intersections.add(_currentBlockX + _currentBlockY);
+					_currentBlockX++;
+					route.add(_currentBlockX + 3 * _currentBlockY);
+				}
+				if (_currentBlockY > _destinationBlockY){
+					intersections.add(_currentBlockX + _currentBlockY);
+					_currentBlockY--;
+					route.add(_currentBlockX + 3 * _currentBlockY);
+				}
+			}
+			else{//going left or up
+				intersections.add(_currentBlockX + _currentBlockY);
+				_currentBlockY--;
+				route.add(_currentBlockX + 3 * _currentBlockY);
+				while (_currentBlockX > _destinationBlockX){
+					intersections.add(_currentBlockX + _currentBlockY);
+					_currentBlockX--;
+					route.add(_currentBlockX + 3 * _currentBlockY);
+				}
+				if (_currentBlockY < _destinationBlockY){
+					intersections.add(_currentBlockX + _currentBlockY);
+					_currentBlockY++;
+					route.add(_currentBlockX + 3 * _currentBlockY);
+				}
+			}	
+		}
+
+
+		//		route.add(3);
+		//		intersections.add(1);
+		//		route.add(4);
+		//		intersections.add(2);
+		//		route.add(5);
+		//		intersections.add(3);
+		//		route.add(2);
 		for (int i=0; i< route.size();i++){
 			Lane lane = Directory.lanes().get(route.get(i));
 			if (i!=0)
 				Directory.intersections().get(i-1).release();
 			for (int j=0; j< lane.permits.size();j++){//TODO change size to the ending position and starting position
-				while(!lane.permits.get(j).tryAcquire());
+				while(!lane.permits.get(j).tryAcquire()){
+					_lookUpDelay.schedule(new TimerTask(){
+						@Override
+						public void run() {
+							_delayForMoving.release();
+						}
+					}, 10);
+
+					try{
+						_delayForMoving.acquire();
+					}
+					catch(InterruptedException e){
+						e.printStackTrace();
+					}
+				}
 				//TODO change waiting to timer based
 				if (lane.isHorizontal){
 					if (lane.xVelocity>0){
@@ -159,10 +214,12 @@ public class CommuterGui implements Gui {
 				if (j!=0)
 					lane.permits.get(j-1).release();
 			}
-			
+
 			if (i<route.size() - 1){
 				Lane next_lane = Directory.lanes().get(route.get(i+1));
-				while(!Directory.intersections().get(i).tryAcquire());
+				while(!Directory.intersections().get(i).tryAcquire()){
+
+				};
 				if (next_lane.isHorizontal){
 					if (next_lane.xVelocity>0){
 						_xDestination = next_lane.xOrigin - 10;
@@ -185,14 +242,18 @@ public class CommuterGui implements Gui {
 				}
 				_transportationMethod = Command.car;
 				waitForLaneToFinish();
-			//TODO to get rid of deadlock acquire both intersection and the first spot in next lane
+				//TODO to get rid of deadlock acquire both intersection and the first spot in next lane
 				lane.permits.get(lane.permits.size()-1).release();
 			}
 			lane.permits.get(lane.permits.size()-1).release();
 		}
-		setPresent(false);
+		//TODO if we have parking area do not set present to false but show in parking lot
+		//setPresent(false);
+		//set automatically
+		//		_currentBlockX = _destinationBlockX;
+		//		_currentBlockY = _destinationBlockY;		
 	}
-	
+
 	//Bus gui
 	public void goToBusStop(BusStopObject busstop){
 		_transportationMethod = Command.walk;
@@ -200,7 +261,7 @@ public class CommuterGui implements Gui {
 		guiMoveFromCurrentPositionTo(destinationP);
 		setPresent(true);
 	}
-/*	
+	/*	
 	//Car gui
 	public void goToCar(CarObject car, Place destination){
 		_goingSomewhere = true;
@@ -208,23 +269,23 @@ public class CommuterGui implements Gui {
 		_yDestination = car.getYPosition();
 		setPresent(true);
 	}
-	
+
 	public void atCar(){
 		setPresent(false);
 		_commuter.msgAtCar();
 	}
-	
-*/	
+
+	 */	
 	public void getOnBus(){
 		setPresent(false);
 	}
-	
+
 	public void getOffBus(BusStopObject busstop){
-	/*	currentPosition = busstop.positionX(); // setx
+		/*	currentPosition = busstop.positionX(); // setx
 		currentPosition = busstop.positionY(); // sety */
 		setPresent(true);
 	}
-	
+
 	//------------------------------------------Animation---------------------------------------
 	@Override
 	public void updatePosition() {
@@ -237,9 +298,9 @@ public class CommuterGui implements Gui {
 			_yPos++;
 		else if (_yPos > _yDestination)
 			_yPos--; 
-		
-		
-		
+
+
+
 		if (_transportationMethod == Command.car){
 			if (_xPos < _xDestination)
 				_xPos++;
@@ -251,7 +312,7 @@ public class CommuterGui implements Gui {
 			else if (_yPos > _yDestination)
 				_yPos--;
 		} 
-		
+
 		if(_xPos == _xDestination &&  _yPos == _yDestination &&
 				(_transportationMethod == Command.car || _transportationMethod == Command.walk)){
 			_transportationMethod = Command.none;
@@ -260,7 +321,7 @@ public class CommuterGui implements Gui {
 			//_commuter.msgReachedDestination();
 		}
 	}
-	
+
 	public void move( int xv, int yv ) {
 		_xPos+=xv;
 		_yPos+=yv;
@@ -276,7 +337,7 @@ public class CommuterGui implements Gui {
 			g.fillRect(_xPos, _yPos, 10, 10);
 		}
 	}
-	
+
 	// ----------------------------------------- UTILITIES --------------------------------------------
 	/** This function returns the x value of the place; it can receive a value of null */
 	private int placeX(Place place) {
@@ -296,53 +357,53 @@ public class CommuterGui implements Gui {
 			return NULL_POSITION_Y;
 		}
 	}
-	
-/*	void move(int destx, int desty){
+
+	/*	void move(int destx, int desty){
 		_xDestination = destx;
 		_yDestination = desty;
 	} */
-	
+
 	void guiMoveFromCurrentPositionTo(Position to){
-        AStarNode aStarNode = (AStarNode)_aStarTraversal.generalSearch(currentPosition, to);
-        List<Position> path = aStarNode.getPath();
-        Boolean firstStep   = true;
-        Boolean gotPermit   = true;
+		AStarNode aStarNode = (AStarNode)_aStarTraversal.generalSearch(currentPosition, to);
+		List<Position> path = aStarNode.getPath();
+		Boolean firstStep   = true;
+		Boolean gotPermit   = true;
 
-        for (Position tmpPath: path) {
-            //The first node in the path is the current node. So skip it.
-            if (firstStep) {
-                firstStep   = false;
-                continue;
-            }
+		for (Position tmpPath: path) {
+			//The first node in the path is the current node. So skip it.
+			if (firstStep) {
+				firstStep   = false;
+				continue;
+			}
 
-            //Try and get lock for the next step.
-            int attempts    = 1;
-            gotPermit       = convertPixelToGridSpace(tmpPath.getX(), tmpPath.getY()).moveInto(_aStarTraversal.getGrid());
+			//Try and get lock for the next step.
+			int attempts    = 1;
+			gotPermit       = convertPixelToGridSpace(tmpPath.getX(), tmpPath.getY()).moveInto(_aStarTraversal.getGrid());
 
-            //Did not get lock. Lets make n attempts.
-            while (!gotPermit && attempts < 3) {
-                //System.out.println("[Gaut] " + guiWaiter.getName() + " got NO permit for " + tmpPath.toString() + " on attempt " + attempts);
+			//Did not get lock. Lets make n attempts.
+			while (!gotPermit && attempts < 3) {
+				//System.out.println("[Gaut] " + guiWaiter.getName() + " got NO permit for " + tmpPath.toString() + " on attempt " + attempts);
 
-                //Wait for 1sec and try again to get lock.
-                try { Thread.sleep(1000); }
-                catch (Exception e){}
+				//Wait for 1sec and try again to get lock.
+				try { Thread.sleep(1000); }
+				catch (Exception e){}
 
-                gotPermit   = convertPixelToGridSpace(tmpPath.getX(), tmpPath.getY()).moveInto(_aStarTraversal.getGrid());
-                attempts ++;
-            }
+				gotPermit   = convertPixelToGridSpace(tmpPath.getX(), tmpPath.getY()).moveInto(_aStarTraversal.getGrid());
+				attempts ++;
+			}
 
-            //Did not get lock after trying n attempts. So recalculating path.            
-            if (!gotPermit) {
-                guiMoveFromCurrentPositionTo(to);
-                break;
-            }
+			//Did not get lock after trying n attempts. So recalculating path.            
+			if (!gotPermit) {
+				guiMoveFromCurrentPositionTo(to);
+				break;
+			}
 
-            //Got the required lock. Lets move.
-            currentPosition.release(_aStarTraversal.getGrid());
-            currentPosition = convertPixelToGridSpace(tmpPath.getX(), tmpPath.getY ());
-          //  move(currentPosition.getX(), currentPosition.getY());
-        }
-        /*
+			//Got the required lock. Lets move.
+			currentPosition.release(_aStarTraversal.getGrid());
+			currentPosition = convertPixelToGridSpace(tmpPath.getX(), tmpPath.getY ());
+			//  move(currentPosition.getX(), currentPosition.getY());
+		}
+		/*
         boolean pathTaken = false;
         while (!pathTaken) {
             pathTaken = true;
@@ -375,13 +436,13 @@ public class CommuterGui implements Gui {
                 }
             }
         }
-        */
-    }
-	
+		 */
+	}
+
 	Position convertPixelToGridSpace(int x, int y){
 		return new Position((x-41)/10, (y-30)/10);
 	}
-	
+
 	private int getBlockX(int xPos){
 		if (xPos >= 41 + 8 * 10 && xPos < 41 + 16 * 10)
 			return 0;
@@ -391,7 +452,7 @@ public class CommuterGui implements Gui {
 			return 2;
 		return -1;
 	}
-	
+
 	private int getBlockY(int yPos){
 		if (yPos >= 30 + 5 * 10 && yPos < 41 + 9 * 10)
 			return 0;
@@ -399,7 +460,7 @@ public class CommuterGui implements Gui {
 			return 1;
 		return -1;
 	}
-	
+
 	private void waitForLaneToFinish() {
 		try {
 			_reachedDestination.acquire();
